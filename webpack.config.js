70e7ec3cc6e1219ea0959832056c8ea96463d29c
David Slone <dbslone@gmail.com>
2018-05-16 15:53:16 -0700

    trying to build

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,61 @@
-var webpack = require('webpack')
-var path = require('path')
+// var webpack = require('webpack')
+// var path = require('path')
+//
+// module.exports = {
+//   entry: './src/index.js',
+//   externals: {
+//     'react': 'commonjs react'
+//   },
+//   output: {
+//     filename: '[name].js',
+//     chunkFilename: '[id].chunk.js',
+//     path: path.resolve(__dirname, 'dist'),
+//     publicPath: '/',
+//     libraryTarget: 'commonjs2',
+//     library: 'ReactToken'
+//   },
+//   optimization: {
+//     minimize: true
+//   },
+//   plugins: [
+//     new webpack.DefinePlugin({
+//       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+//     })
+//   ],
+//   module: {
+//     rules: [
+//       { test: /\.js?$/, exclude: /node_modules/, use: { loader: 'babel-loader' } }
+//     ]
+//   }
+// }
 
-var reactExternal = {
-  root: 'React',
-  commonjs2: 'react',
-  commonjs: 'react',
-  amd: 'react'
-}
-var reactDOMExternal = {
-  root: 'ReactDOM',
-  commonjs2: 'react-dom',
-  commonjs: 'react-dom',
-  amd: 'react-dom'
-}
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: './src/index.js',
+  output: {
+    filename: '[name].js',
+    libraryTarget: 'commonjs2'
+  },
   externals: {
     'react': 'commonjs react'
   },
-  output: {
-    filename: '[name].js',
-    chunkFilename: '[id].chunk.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
-    libraryTarget: 'commonjs2',
-    library: 'ReactToken'
-  },
-  optimization: {
-    minimize: true
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-    })
-  ],
   module: {
     rules: [
-      { test: /\.js?$/, exclude: /node_modules/, use: { loader: 'babel-loader' } }
+      {
+        test: /\.(js|jsx)$/,
+        use: "babel-loader",
+        exclude: /node_modules/
+      }
     ]
+  },
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "docs"),
+    port: 8000,
+    stats: "minimal"
   }
 }
